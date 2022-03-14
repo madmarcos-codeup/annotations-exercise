93e374141e139585a6ec3462654841a9a2f8f135
docrob <mark.robinson@utsa.edu>
2022-03-14 16:17:34 -0500

    final fix

--- src/Author.java
+++ src/Author.java
@@ -1,17 +1,12 @@
-/**
- * This source file is subject to the license that is bundled with this package in the file LICENSE.
- */
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("all")
 public class Author extends Person {
-    @SuppressWarnings("unchecked")
-    private List books;
+    private final List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -30,7 +25,4 @@ public class Author extends Person {
         books.add(book);
     }
 
-    public String sortName() {
-        return String.format("%s, %s", lastName, firstName);
-    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,7 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
-@SuppressWarnings("all")
+
 public class Person {
     protected String firstName;
     protected String lastName;
@@ -11,7 +11,4 @@ public class Person {
         this.lastName = lastName;
     }
 
-    public String fullName() {
-        return String.format("%s %s", firstName, lastName);
-    }
 }
